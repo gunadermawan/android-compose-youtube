86d15e18180468ed959a3dceb608dc2c75f7ad0c
gunadermawan <gunadermawan.official@gmail.com>
2023-07-20 08:24:39 +0700

    update code and add image_user.png

--- app/src/main/java/com/gunder/basic_layouts/MainActivity.kt
+++ app/src/main/java/com/gunder/basic_layouts/MainActivity.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,7 +27,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gunder.basic_layouts.ui.theme.BasiclayoutsTheme
@@ -78,5 +92,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BasiclayoutsTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun UserChat(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = {})
+            .padding(4.dp),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.image_user),
+            contentDescription = "image user",
+            modifier = modifier
+                .padding(4.dp)
+                .border(2.dp, Color.Blue, CircleShape)
+                .clip(CircleShape)
+                .size(60.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Guna Dermawan", fontWeight = FontWeight.Bold)
+            Text(text = "How are you today?", modifier = Modifier.offset(x = 8.dp))
+            Icon(
+                imageVector = Icons.Filled.CheckCircle,
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserChatPreview() {
+    BasiclayoutsTheme {
+        UserChat(name = "Guna Dermawan")
     }
 }
